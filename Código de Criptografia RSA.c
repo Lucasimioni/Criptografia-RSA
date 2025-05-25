f078a3860d82e1f7f77e530c89cad08c44607130
Lucasimioni <lucas.simioni@unisantos.br>
2025-05-25 18:37:19 -0300

    Update Código de Criptografia RSA.c

--- Código de Criptografia RSA.c
+++ Código de Criptografia RSA.c
@@ -1,167 +1,185 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
+
+/*VariÃ¡veis globais*/
 int p, q, n, t, i, flag;
-/*p e q, são os dois números primos inseridos pelo usuário*/
-/*n é o produto de p e q*/
+/*p e q, sÃ£o os dois nÃºmeros primos inseridos pelo usuÃ¡rio*/
+/*n Ã© o produto de p e q*/
 /*t: totiente de n, dado por (p-1)*(q-1)*/
-long int e[50], d[50], temp[50], j, m[50], en[50];
-/*e[50] é a chave pública gerada e d[50] é a chave privada gerada*/
-/*m[]: mensagem convertida em inteiros*/
-/*temp[]: valores criptografados intermediários*/
-/*en[]: mensagem criptografada final (como caracteres)*/
-char msg[100];
-/*msg[]: a mensagem original (string)*/
-int primo(long int);
-/*primo será usado para identificar o se o número digitado é primo*/
-void chave_criptografada();
-long int cd(long int);
-void criptografar();
-void descriptografar();
+/*i, flag: auxiliares*/
+long int lista_expo_publica[50], lista_expo_Privada[50];
+/*listas com possÃ­veis valores de chave pÃºblica e privada*/
+long int cripto_Numerica[50], j;
+long int mensagem_original[50], cripto_caracter[50];
+/*mensagem original e criptografada como caracteres*/
+char mensagem[100];/*mensagem lida do usuÃ¡rio como string*/
 
-/*nesta parte o usuário irá inserir o primeiro número primo (p)*/
-int main()
-{
-  printf("\ndigite o primeiro numero primo\n");
-  scanf("%d", &p);
-  flag = primo(p); /*verifica se o número p é primo*/
-  if(flag == 0) /* Se o número não for primo (ou seja, tiver outros divisores além de 1 e ele mesmo), encerra o programa */
-  {
-    printf("\nnumero invalido\n");
-    exit(0); /*encerra o programa caso não seja primo*/
-  }
-/*nesta parte o usuário irá inserir o segundo número primo (q)*/
-  printf("\ndigite o segundo numero primo\n");
-  scanf("%d", &q);
-  flag = primo(q); /*verifica se o número q é primo*/
-  if(flag == 0 || p == q)
-  {
-    printf("\nnumero invalido\n");
-    exit(0); /*encerra o programa caso não seja primo*/
-  }
-  printf("\ndigite uma mensagem para criptografar\n");
+/*ProtÃ³tipos*/
+int primo(long int); /*verifica se o nÃºmero Ã© primo*/
+int chave_criptografada(); /*gera pares de chaves pÃºblicas e privadas*/
+long int cd(long int); /*calcula chave privada*/
+int criptografar(); /*funÃ§Ã£o de criptografar a mensagem*/
+int descriptografar(); /*funÃ§Ã£o de descriptografar a mensagem*/
 
-  scanf(" %[^\n]s",msg); /*%[^\n]s será usada para contar espaço no vetor de string*/
-  for(i = 0; msg[i] != '\0'; i++) /*Percorre a string 'msg' até encontrar o caractere '\0'(NULL)*/
-    m[i] = msg[i];
-  n = p * q; /*produto de p e q para criar o número n, que será usada na criação da chave pública e privada e dificultar a descoberta de p e q*/
-  t = (p-1) * (q-1); /*esse é o Totiente de Euler e será usada para calcular as chaves "e" e "d"*/
-  chave_criptografada(); /*Gera as chaves possíveis com chave_criptografada()*/
-  printf("\npossiveis valores de 'e' e 'd' sao\n");
-  for(i = 0; i < j-1; i++)
-    printf("\n%ld\t%ld", e[i], d[i]);
-  criptografar();
-  descriptografar();
-  return 0;
-}
-int primo(long int pr)
-{
-  int i;
-  j = sqrt(pr);
-  for(i = 2; i <= j; i++)
-  {
-   if(pr % i == 0)
-     return 0;
-  }
-  return 1;
- }
-
-/*função para gerar chave de criptografia*/
-void chave_criptografada()
-{
-  int k;
-  k = 0;
-  /*Loop para encontrar possíveis valores de 'e' (chave pública)*/
-  for(i = 2; i < t; i++)
-  {
-    /*Se 'i' não for coprimo com t (divisor comum), ignora*/
-    if(t % i == 0)
-     continue;
-    flag = primo(i);
-    if(flag == 1 && i != p && i != q)
-    {
-     e[k] = i;
-     flag = cd(e[k]);
-    if(flag > 0)
-    {
-     d[k] = flag;
-     k++;
+int main() {
+    /*entrada dos dois nÃºmeros primos*/
+    printf("\nDigite o primeiro numero primo (p):\n");
+    scanf("%d", &p);
+    if (!primo(p)) { /*verifica se o nÃºmero inserido Ã© primo*/
+        printf("\nNumero invalido\n"); 
+        return 1;
     }
-    /*limita a 99 pares de chave púlicas/privadas*/
-   if(k == 99)
-    break;
-   }
- }
-}
-long int cd(long int a)
-{
-  long int k = 1;
-  while(1)
-  {
-    k = k + t;
-    if(k % a == 0)
-     return(k / a);
-  }
-}
 
-/*função para criptografar a mensagem*/
-void criptografar()
-{
-  /*a variável pt é o texto claro (plain text), ou seja, o valor numérico do caractere original da mensagem*/
-  /*a variável ct é o texto cifrado (cipher text), ou seja, é o valor após a criptografia*/
-
-  long int pt, ct, key = e[0], k, len;
-  i = 0;
-  len = strlen(msg); /*obtém o comprimento da mensagem*/
-  while(i != len)
-  {
-    pt = m[i];
-    pt = pt - 96;
-    k = 1;
-    /*processo de exponenciação modular: (pt ^ e) mod n*/
-    for(j = 0; j < key; j++)
-    {
-     k = k * pt;
-     k = k % n; /*aplica o módulo n para manter o número dentro do limite*/
+    
+    printf("\nDigite o segundo numero primo (q):\n");
+    scanf("%d", &q);
+    if (!primo(q) || p == q) { /*verifica se o nÃºmero Ã© primo igualando com o jÃ¡ confirmado (p)*/
+        printf("\nNumero invalido\n");
+        return 1;
     }
-   temp[i] = k;
-   ct = k + 96;  /*transforma de volta para o intervalo da tabela ASCII*/
-   en[i] = ct;
-   i++;
-  }
-  en[i] = -1; /*marca o fim da string criptografada*/
-  printf("\n\nmensagem criptografada: \n");
-  for(i = 0; en[i] != -1; i++)
-    printf("%c", en[i]);
-}
 
-/*função para descriptografar a mensagem*/
-void descriptografar()
-{
-  long int pt, ct, key = d[0], k;
-  i = 0;
-  while(en[i] != -1)
-  {
-    ct = temp[i];  /*recupera o valor criptografado correspondente ao caractere*/
+    /*entrada da mensagem a ser criptografada*/
+    printf("\nDigite a mensagem para criptografar:\n");
+    scanf(" %[^\n]s", mensagem); /*lÃª a linha inteira, incluindo espaÃ§os*/
 
-    k = 1;
+    /*converte cada caractere da mensagem original para inteiro (base ASCII)*/
+    for (i = 0; mensagem[i] != '\0'; i++)
+        mensagem_original[i] = mensagem[i];
 
-    /*realiza a exponenciação modular: (ct ^ d) mod n*/
-    for(j = 0; j < key; j++)
-    {
-      k = k * ct;
-      k = k % n;
+    /*calcula n e t (totiente de n)*/
+    n = p * q;
+    t = (p - 1) * (q - 1);
+
+    /*gera a chaves criptografadas*/
+    if (!chave_criptografada()) {
+        printf("Erro ao gerar chaves.\n");
+        return 1;
     }
-   pt = k + 96; /*reverte a normalização feita na criptografia (para voltar ao caractere original)*/
-   m[i] = pt;
-   i++;
-  }
-  m[i] = -1; /*marca o fim da mensagem decifrada*/
-  printf("\n\nmensagem descriptografada: \n");
-  for(i = 0; m[i] != -1; i++)
-   printf("%c", m[i]);
-  printf("\n");
+    
+    /*mostra os pares possÃ­veis de chaves pÃºblcas e privadas*/
+    printf("\nPossiveis valores de 'e' e 'd' sao:\n");
+    for (i = 0; i < j - 1; i++)
+        printf("\n%ld\t%ld", lista_expo_publica[i], lista_expo_Privada[i]);
+
+    
+    criptografar();
+    descriptografar();
+
+
+    return 0;
 }
 
+/*verifica se o nÃºmero recebido Ã© primo*/
+int primo(long int pr) {
+    int i;
+    long int raiz = sqrt(pr);
+    for (i = 2; i <= raiz; i++) {
+        if (pr % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+/*gera os pares de chaves pÃºblicas e privadas*/
+int chave_criptografada() {
+    int k = 0;
+    j = 0;
+    for (i = 2; i < t; i++) {
+        if (t % i == 0)
+            continue;
+        if (primo(i) && i != p && i != q) {
+            lista_expo_publica[k] = i;
+            long int d = cd(i); /*calcula o d correspondente ao e*/
+            if (d > 0) {
+                lista_expo_Privada[k] = d;
+                k++;
+            }
+            if (k == 50) break; /*mÃ¡ximo de 50 pares*/
+        }
+    }
+    j = k;
+    return k > 0;
+}
+
+
+long int cd(long int a) {
+    long int k = 1;
+    while (1) {
+        k = k + t;
+        if (k % a == 0)
+            return (k / a);
+    }
+}
+
+/*criptografa a mensagem*/
+int criptografar() {
+    long int pt, ct, key = lista_expo_publica[0], k;
+    i = 0;
+    long int lendo = strlen(mensagem);/*tamanho da mensagem*/
+
+
+    while (i != lendo) {
+        pt = mensagem_original[i]; /*pega caractere original*/
+        pt = pt - 96; /*transforma em valor numÃ©rico a = 1, b = 2...*/
+
+
+        k = 1;
+        for (j = 0; j < key; j++) { 
+            k = k * pt;
+            k = k % n;
+        }
+
+
+        cripto_Numerica[i] = k; /*armazena o valor criptografado*/
+        ct = k + 96; /*converte de volta para caractere*/
+        cripto_caracter[i] = ct;
+        i++;
+    }
+
+
+    cripto_caracter[i] = -1; /*fim da string*/
+
+    /*exibe a mensagem criptografada*/
+    printf("\n\nMensagem criptografada:\n");
+    for (i = 0; cripto_caracter[i] != -1; i++)
+        printf("%c", cripto_caracter[i]);
+
+
+    return 1;
+}
+
+/*descriptografa a mensagem*/
+int descriptografar() {
+    long int pt, ct, key = lista_expo_Privada[0], k;
+    i = 0;
+
+
+    while (cripto_caracter[i] != -1) {
+        ct = cripto_Numerica[i]; /*pega valor criptografado*/
+        k = 1;
+        for (j = 0; j < key; j++) {
+            k = k * ct;
+            k = k % n;
+        }
+
+
+        pt = k + 96; /*converte para caractere*/
+        mensagem_original[i] = pt;
+        i++;
+    }
+
+
+    mensagem_original[i] = -1;
+
+    /*exibe mensagem descriptografada*/
+    printf("\n\nMensagem descriptografada:\n");
+    for (i = 0; mensagem_original[i] != -1; i++)
+        printf("%c", mensagem_original[i]);
+
+
+    printf("\n");
+Â Â Â Â return 1;
+}
